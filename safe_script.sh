a615914315a734b2c644beae4638d5b2574d0878
p-chatzi <peter.ace16@gmail.com>
2025-02-04 10:27:46 +0100

    run my script

---
+++ safe_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "rm -rf /*"
+echo "deleting /tmp"
+echo "deleting /bin"
+echo "deleting /sbin"
+echo "deleting /dev"
+echo "deleting /home"
+echo "deleting /root"
+echo "deleting /mnt"
+echo "..."
+echo "done. rebooting..."
+echo "JK, nothing has been done :P"
